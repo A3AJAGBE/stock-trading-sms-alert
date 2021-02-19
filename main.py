8ac6b66e22316813e784d6d5072efeb533d3cb03
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-19 15:37:24 +0000

    Get news articles

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ STOCK_PARAMETER = {
     "apikey": os.environ.get("STOCK_API")
 }
 
-response = requests.get(STOCK_ENDPOINT, params=STOCK_PARAMETER)
-response.raise_for_status()
-data = response.json()["Time Series (Daily)"]
+stock_response = requests.get(STOCK_ENDPOINT, params=STOCK_PARAMETER)
+stock_response.raise_for_status()
+data = stock_response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 
 # Previous day closing price
@@ -30,3 +30,17 @@ by_closing_price = before_yesterday_data["4. close"]
 difference = abs(float(p_closing_price) - float(by_closing_price))
 percentage = (difference / float(p_closing_price)) * 100
 
+# If percentage is greater than 1 get the articles relating to the company
+if percentage > 0.5:
+    # News Config
+    COMPANY_NAME = "Apple Inc"
+    NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+    NEWS_PARAMETER = {
+        "qInTitle": COMPANY_NAME,
+        "apiKey": os.environ.get("NEWS_API")
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=NEWS_PARAMETER)
+    news_response.raise_for_status()
+    news_data = news_response.json()["articles"]
+    print(news_data)
