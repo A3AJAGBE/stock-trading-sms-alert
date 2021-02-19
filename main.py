5d22e4767f64d5851aed5943a313c9670810e776
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-19 15:22:39 +0000

    Get diff and percentage

--- main.py
+++ main.py
@@ -21,9 +21,12 @@ data_list = [value for (key, value) in data.items()]
 # Previous day closing price
 previous_day_data = data_list[0]
 p_closing_price = previous_day_data["4. close"]
-print(p_closing_price)
 
 # Get the closing price for 2-days back (Day before yesterday)
 before_yesterday_data = data_list[1]
 by_closing_price = before_yesterday_data["4. close"]
-print(by_closing_price)
+
+# Get the difference and percentage
+difference = abs(float(p_closing_price) - float(by_closing_price))
+percentage = (difference / float(p_closing_price)) * 100
+
